5373c60e2753a287c6bf1f29690120cf9bbeb799
mtaisboss <mohammad.1387.kavian@gmail.com>
2025-06-25 00:11:18 +0330

    edited pyramid name and inherited mesh class

--- Game Engine/Pyramid.py
+++ Game Engine/Pyramid.py
@@ -1,7 +1,7 @@
 from Vector import Vector
 from Transform import Transform
-
-class Ethereum:
+from Mesh import Mesh
+class Pyramid(Mesh):
     def __init__(self,transform = Transform()):
         self.transform = transform
         pyramid_vectors = []
