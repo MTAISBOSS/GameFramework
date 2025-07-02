0177a11988951f5da6d1e899738f028f431c2d77
mtaisboss <mohammad.1387.kavian@gmail.com>
2025-07-02 16:21:08 +0330

    added a simple input configuration, will implement inshallah

---
+++ Game Engine/Configs/InputConfiguration.py
@@ -0,0 +1,7 @@
+from dearpygui import dearpygui as dpg
+config ={
+    "Left" : (dpg.mvKey_A,dpg.mvKey_Left),
+    "Right" : (dpg.mvKey_D,dpg.mvKey_Right),
+    "Up" : (dpg.mvKey_W,dpg.mvKey_Up),
+    "Down" : (dpg.mvKey_S,dpg.mvKey_Down),
+}
--- Game Engine/Input.py
+++ Game Engine/Input.py
@@ -1,3 +1,9 @@
+from dearpygui import dearpygui as dpg
+from Configs.InputConfiguration import config
+import json
 class Input:
     def __init__(self):
-        pass
+        self.input = config
+        
+    
+
